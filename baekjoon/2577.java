e16de7240304f634e3d4b81d969e28ad8ed56a27
Hyejin Lee <hyejin.lee0805@gmail.com>
2017-10-13 15:50:33 +0900

    Create 2577.java

---
+++ baekjoon/2577.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		//Scanner sc = new Scanner(new FileInputStream("input.txt"));
+		Scanner sc = new Scanner(System.in);
+		
+		int[] arr = new int[10];
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+    char[] multi = String.valueOf(a*b*c).toCharArray();
+		
+		for(int i=0; i<multi.length; i++)
+			arr[multi[i]-'0'] += 1;
+		
+		for(int i=0; i<arr.length; i++)
+			System.out.println(arr[i]);
+	}
+}
